525de7410e5bd37d081a8ad07948b59238af8cf0
YoungBaBu <lagiosaniko@gmail.com>
2025-06-07 13:10:52 +0400

    yes

---
+++ BookstoreTry2/Data/BookstoreContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using BookstoreTry2.Models;
+
+namespace BookstoreTry2.Data
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("books");
+        }
+    }
+}
---
+++ BookstoreTry2/Models/Book.cs
@@ -0,0 +1,12 @@
+namespace BookstoreTry2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublishedYear { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
